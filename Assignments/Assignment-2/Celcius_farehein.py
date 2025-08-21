b27f22a856ee59dceea0d25ac244c2e7fcde7a9d
sakshi-13421 <sakshishirke86@gmail.com>
2025-08-21 21:00:05 +0530

    Delete Assignments/Assignment-2 directory

--- Assignments/Assignment-2/Celcius_farehein.py
+++
@@ -1,5 +0,0 @@
-celcius = int(input("Enter a celcius="))
-
-Fahrehin = (celcius*9/5)+32
-
-print(f"Celcius to farehein is {Fahrehin}")
--- Assignments/Assignment-2/hh_mm_ss.py
+++
@@ -1,7 +0,0 @@
-hh=int(input("Enter a hour="))
-mm=int(input("Enter a minute="))
-ss=int(input("Enter a second="))
-
-Total_SS = (hh*3600) + (mm*60) + ss
-
-print(f"Total second is {Total_SS}")
