600bc116f4a794488888c38a990e6f0b9c6fdfa6
sakshi-13421 <sakshishirke86@gmail.com>
2025-08-21 12:52:09 +0530

    Add files via upload

---
+++ Test/Test-1/Simple_Interest.py
@@ -0,0 +1,7 @@
+P = int(input("Enter a principle amount="))
+R = int(input("Enter a rate of interest="))
+T = int(input("Enter a time="))
+
+Simple_Interest=P*R*T/100
+
+print(f"Simple Interst is {Simple_Interest}")
