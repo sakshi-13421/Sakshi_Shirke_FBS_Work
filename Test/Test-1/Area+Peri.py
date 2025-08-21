07fe20ba83bc406cdca5f9a62a09ee75f243bdfe
sakshi-13421 <sakshishirke86@gmail.com>
2025-08-21 12:54:07 +0530

    Update Area+Peri.py

--- Test/Test-1/Area+Peri.py
+++ Test/Test-1/Area+Peri.py
@@ -2,8 +2,9 @@ length=int(input("Enter a length="))
 breadth=int(input("Enter a breadth="))
 radius=int(input("Enter a radius="))
 
-area = length*breadth+1/2*3.14*radius**2
+area = length*breadth+1/2*3.14*radius*radius
 perimeter = 2*length+breadth+3.14*radius
 
 print(f"Area of rec {area}")
+
 print(f"Perimeter of rec {perimeter}")
