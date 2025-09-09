01d001fb3222f4a0038eef61fb5fc8459752d936
sakshi-13421 <sakshishirke86@gmail.com>
2025-09-09 10:13:05 +0530

    Update Q5.py

--- Assignments/Assignment-4/Q5.py
+++ Assignments/Assignment-4/Q5.py
@@ -1,7 +1,8 @@
 no = int(input("Enter a no : "))
-a=0
-b=1
-print("Fibonacii Series")
-for i in range(no):
-    print(a, end = " ")
-    a, b = b , a + b
+a = -1
+b = 1
+    for i in range(no):
+        c = a + b
+        print(c, end = " ")
+        a, b = b, c
+
