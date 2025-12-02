d312e457ce8d921188ba8e7c3c381456a3da1528
Sakshi Shirke <sakshishirke86@gmail.com>
2025-12-02 13:00:42 +0530

    Delete Test directory

--- Test/Test-7/Q2.py
+++
@@ -1,5 +0,0 @@
-strings = ['sakshi', 'siddhi', 'sumit']
- 
-upper_list= [list.upper() for list in strings]
-print("Upper case list is:")
-print(upper_list)
--- Test/Test-7/Q3.py
+++
@@ -1,3 +0,0 @@
-odd_no = [no for no in range(1,51) if no % 2 != 0]
-print("Display the list of odd number 1 to 50:")
-print(odd_no)
